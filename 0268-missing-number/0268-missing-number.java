c8170396c2ebcf3b4d1a5157300be00a2310eb01
Basupally Karthik Reddy <basupallykarthikreddy@gmail.com>
2023-09-21 23:21:06 +0530

    Time: 0 ms (100.00%), Space: 43.2 MB (99.60%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,13 +1,16 @@
 class Solution {
     public int missingNumber(int[] nums) {
-               int x=0;
-       for(int l:nums){
-           x^=l;
-       }
-       for(int i=0;i<=nums.length;i++){
-           x^=i;
-       }
-       return x;
-        
+        int[] arr=new int[nums.length+1];
+        for (int i=0;i<arr.length;i++){
+            arr[i]=i;
+        }
+        int xor = 0;
+        for (int i = 0; i <= nums.length; i++) {
+            xor ^= i;
+            if (i < nums.length) {
+                xor ^= nums[i];
+            }
+        }
+        return xor;
     }
 }
