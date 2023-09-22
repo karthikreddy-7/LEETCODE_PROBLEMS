9e5c71bb1818a1505f14508960ad16bfc3c06f9d
Basupally Karthik Reddy <basupallykarthikreddy@gmail.com>
2023-09-22 14:11:51 +0530

    Time: 2 ms (66.33%), Space: 53.5 MB (30.34%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,36 +1,27 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int n=nums.length;
-        int[] res=new int[n];
-        int[] prefix=new int[n];
-        int[] postfix=new int[n];
-        int prod=1;
-        for (int i=0;i<n;i++){
-            prod*=nums[i];
-            prefix[i]=prod;
+        int[]left = new int[nums.length];
+        int[]right = new int[nums.length];
+        int[]ans = new int[nums.length];
+
+        left[0] = 1 ;
+        for(int i = 1;i<nums.length;i++){
+            left[i] = left[i-1]*nums[i-1];   
         }
-        int prod1=1;
-        for (int i=n-1;i>=0;i--){
-            prod1*=nums[i];
-            postfix[i]=prod1;
+
+        right[nums.length-1] = 1;
+        for(int i = nums.length-2;i>=0;i--){
+            right[i] = right[i+1]*nums[i+1];   
         }
-        for (int i=0;i<n;i++){
-            int preprod=1;
-            if (i==0){
-                preprod=1;
-            }
-            else{
-                preprod=prefix[i-1];
-            }
-            int postprod=1;
-            if (i==n-1){
-                postprod=1;
-            }
-            else{
-                postprod=postfix[i+1];
-            }
-            res[i]=preprod*postprod;
+
+         for(int i = 0;i<nums.length;i++){
+            ans[i] = left[i]*right[i];   
         }
-        return res;
+
+    return ans;
+
+
+
+       
     }
 }
