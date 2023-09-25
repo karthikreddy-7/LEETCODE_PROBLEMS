91c211d682f70a755ca72dba947ef67273949007
Basupally Karthik Reddy <basupallykarthikreddy@gmail.com>
2023-09-25 19:46:44 +0530

    Time: 2 ms (76.13%), Space: 60.4 MB (86.52%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,12 +1,16 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int maxprofit=0;
-        int minimumsofar=prices[0];
+        int minsofar=prices[0];
         for (int i=0;i<prices.length;i++){
-            minimumsofar=Math.min(minimumsofar,prices[i]);
-            maxprofit=Math.max(maxprofit,(prices[i]-minimumsofar));
+            if (prices[i]<minsofar){
+                minsofar=prices[i];
+            }
+            int profit=prices[i]-minsofar;
+            if (profit>maxprofit){
+                maxprofit=profit;
+            }
         }
         return maxprofit;
-        
     }
 }
