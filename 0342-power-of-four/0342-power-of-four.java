c88c659c92bed99838627c5a36a92bf6ab199974
Basupally Karthik Reddy <basupallykarthikreddy@gmail.com>
2023-10-23 10:54:18 +0530

    Time: 0 ms (100.00%), Space: 39.8 MB (19.65%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,14 +1,13 @@
-class Solution {
+public class Solution {
     public boolean isPowerOfFour(int n) {
-        if (n <= 0) {
-            return false;
-        }
-        while (n > 1) {
-            if (n % 4 != 0) {
-                return false;
+        if (n > 0 && (n & (n - 1)) == 0) {
+            int zero_count = 0;
+            while (n > 1) {
+                zero_count++;
+                n >>= 1;
             }
-            n /= 4;
+            return zero_count % 2 == 0;
         }
-        return true;
+        return false;
     }
 }
