e668a1b7bf684543cac108ed1eb9c5267af58079
Basupally Karthik Reddy <basupallykarthikreddy@gmail.com>
2023-10-27 22:27:08 +0530

    Time: 17 ms (66.12%), Space: 43 MB (56.65%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,36 +1,53 @@
-public class Solution {
+class Solution {
     public String longestPalindrome(String s) {
-        if (s.length() <= 1) {
-            return s;
+        // Check if the input string is empty, return an empty string if so
+        if (s.isEmpty())
+            return "";
+
+        // Initialize variables to store the indices of the longest palindrome found
+        int[] longestPalindromeIndices = { 0, 0 };
+
+        // Loop through the characters in the input string
+        for (int i = 0; i < s.length(); ++i) {
+            // Find the indices of the longest palindrome centered at character i
+            int[] currentIndices = expandAroundCenter(s, i, i);
+
+            // Compare the length of the current palindrome with the longest found so far
+            if (currentIndices[1] - currentIndices[0] > longestPalindromeIndices[1] - longestPalindromeIndices[0]) {
+                // Update the longest palindrome indices if the current one is longer
+                longestPalindromeIndices = currentIndices;
+            }
+
+            // Check if there is a possibility of an even-length palindrome centered at
+            // character i and i+1
+            if (i + 1 < s.length() && s.charAt(i) == s.charAt(i + 1)) {
+                // Find the indices of the longest even-length palindrome centered at characters
+                // i and i+1
+                int[] evenIndices = expandAroundCenter(s, i, i + 1);
+
+                // Compare the length of the even-length palindrome with the longest found so
+                // far
+                if (evenIndices[1] - evenIndices[0] > longestPalindromeIndices[1] - longestPalindromeIndices[0]) {
+                    // Update the longest palindrome indices if the even-length one is longer
+                    longestPalindromeIndices = evenIndices;
+                }
+            }
         }
 
-        int maxLen = 1;
-        String maxStr = s.substring(0, 1);
-        s = "#" + s.replaceAll("", "#") + "#";
-        int[] dp = new int[s.length()];
-        int center = 0;
-        int right = 0;
+        // Extract and return the longest palindrome substring using the indices
+        return s.substring(longestPalindromeIndices[0], longestPalindromeIndices[1] + 1);
+    }
 
-        for (int i = 0; i < s.length(); i++) {
-            if (i < right) {
-                dp[i] = Math.min(right - i, dp[2 * center - i]);
-            }
-
-            while (i - dp[i] - 1 >= 0 && i + dp[i] + 1 < s.length() && s.charAt(i - dp[i] - 1) == s.charAt(i + dp[i] + 1)) {
-                dp[i]++;
-            }
-
-            if (i + dp[i] > right) {
-                center = i;
-                right = i + dp[i];
-            }
-
-            if (dp[i] > maxLen) {
-                maxLen = dp[i];
-                maxStr = s.substring(i - dp[i], i + dp[i] + 1).replaceAll("#", "");
-            }
+    // Helper function to find and return the indices of the longest palindrome
+    // extended from s[i..j] (inclusive) by expanding around the center
+    private int[] expandAroundCenter(final String s, int i, int j) {
+        // Expand the palindrome by moving outward from the center while the characters
+        // match
+        while (i >= 0 && j < s.length() && s.charAt(i) == s.charAt(j)) {
+            i--; // Move the left index to the left
+            j++; // Move the right index to the right
         }
-
-        return maxStr;
+        // Return the indices of the longest palindrome found
+        return new int[] { i + 1, j - 1 };
     }
 }
