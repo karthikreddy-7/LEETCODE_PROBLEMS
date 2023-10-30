b882309e00834072c4ca55380307069693be99e6
Basupally Karthik Reddy <basupallykarthikreddy@gmail.com>
2023-10-30 09:30:12 +0530

    Time: 10 ms (42.50%), Space: 43.8 MB (53.28%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
@@ -1,34 +1,11 @@
-class Solution {
+public class Solution {
     public int[] sortByBits(int[] arr) {
-        // Convert the input integer array to Integer objects for sorting
-        Integer[] integerArr = Arrays.stream(arr).boxed().toArray(Integer[]::new);
-
-        // Create a custom comparator to sort by bit count and then numerically
-        Comparator<Integer> comparator = new BitCountComparator();
-
-        // Sort the array using the custom comparator
-        Arrays.sort(integerArr, comparator);
-
-        // Convert the sorted array back to primitive integers
-        int[] sortedArr = Arrays.stream(integerArr).mapToInt(Integer::intValue).toArray();
-
-        return sortedArr;
-    }
-}
-
-class BitCountComparator implements Comparator<Integer> {
-    @Override
-    public int compare(Integer a, Integer b) {
-        // Compare based on the count of set bits (1s) in the binary representation
-        int bitCountA = Integer.bitCount(a);
-        int bitCountB = Integer.bitCount(b);
-
-        if (bitCountA == bitCountB) {
-            // If bit counts are the same, compare numerically
-            return a - b;
-        } else {
-            // Sort by the number of set bits in ascending order
-            return bitCountA - bitCountB;
-        }
+        Integer[] boxedArray = Arrays.stream(arr).boxed().toArray(Integer[]::new);
+        Arrays.sort(boxedArray, (a, b) -> {
+            int countA = Integer.bitCount(a);
+            int countB = Integer.bitCount(b);
+            return countA == countB ? a - b : countA - countB;
+        });
+        return Arrays.stream(boxedArray).mapToInt(Integer::intValue).toArray();
     }
 }
