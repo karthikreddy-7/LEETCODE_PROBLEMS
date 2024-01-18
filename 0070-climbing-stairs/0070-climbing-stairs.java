bcc480ba8fcbacf2a81d48f7389fd7488b4caaee
Basupally Karthik Reddy <basupallykarthikreddy@gmail.com>
2024-01-18 08:31:38 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (47.63%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,23 +1,18 @@
-import java.util.HashMap;
-
 class Solution {
     public int climbStairs(int n) {
-        return helper(n, new HashMap<>());
+        HashMap<Integer,Integer> map=new HashMap<>();
+        return helper(n,map);
     }
-
-    public int helper(int n, HashMap<Integer, Integer> memo) {
-        if (n == 0) {
-            return 1;
+    public int helper(int n, HashMap<Integer, Integer> map) {
+        if (n == 0) return 1;
+        if (n < 0) return 0;
+        
+        if (map.containsKey(n)) {
+            return map.get(n);
         }
-        if (n < 0) {
-            return 0;
-        }
-        if (memo.containsKey(n)) {
-            return memo.get(n);
-        }
-
-        int ways = helper(n - 1, memo) + helper(n - 2, memo);
-        memo.put(n, ways); // Memoize the result for n
-        return ways;
+        
+        int steps = helper(n - 1, map) + helper(n - 2, map);
+        map.put(n, steps);
+        return steps;
     }
 }
